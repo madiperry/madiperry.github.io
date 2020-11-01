8851636c7d4d433f2144e783ee3a6e8871f04c5e
madiperry <madiperry@byui.edu>
2020-10-31 22:13:00 -0500

    css

--- lesson7/js/pic.js
+++ lesson7/js/pic.js
@@ -1,32 +1,27 @@
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0 0 50px 0"
-};
-
-function loadImages(image) {
+const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => { image.removeAttribute('data-src'); 
+  image.onload = () => { 
+    image.removeAttribute('data-src'); 
 };
 };
 
 if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
+    const imgobserver = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
-        if(!item.isIntersecting) {
-          return;
-        } else{
+        if(item.isIntersecting) {
           loadImages(item.target);
-          imgobserver.unobserve(item.target);
+          imageobserver.unobserve(item.target);
         }
       });
     });
+
     imagesToLoad.forEach((img) => {
-      imgobserver.observe(img);
+      imageobserver.observe(img);
     });
   } else {
     imagesToLoad.forEach((img) => {
-      loadImages(img);
+      loadImages(img)
     });
   }
