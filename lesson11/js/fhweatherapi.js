5f09716675bae6aa113091db0ce7b11e44b2f916
madiperry <madiperry@byui.edu>
2020-11-30 21:23:24 -0600

    api update

---
+++ lesson11/js/fhweatherapi.js
@@ -0,0 +1,53 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=42.0380399&lon=-111.4048681&units=imperial&APPID=40bfc11ee6cf5d36b6419656660b977d";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=42.0380399&lon=-111.4048681&units=imperial&APPID=40bfc11ee6cf5d36b6419656660b977d";
+
+fetch(apiURL)
+    .then(response => response.json())
+    .then(jsObject => {
+    
+        const curcon = document.querySelector('#curcon');
+        const curtemp = document.querySelector('#current-temp');
+        const hum = document.querySelector('#humidity');
+        const ws= document.querySelector('#winds');
+
+        curtemp.innerHTML = (Math.round(jsObject.main.temp));
+        hum.innerHTML = jsObject.main.humidity;
+        ws.innerHTML = jsObject.wind.speed;
+        curcon.innerHTML = jsObject.weather[0].main;
+
+        let t = (parseFloat(jsObject.main.temp));
+        let s = (parseFloat(jsObject.main.speed));;
+        let chill="N/A";
+        if (t <= 50 && s >= 3) {
+            let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow (s, 0.16))));
+            chill=Math.round(f);
+        } else{
+            chill="N/A";
+        }
+    });
+
+fetch(forecastURL)
+    .then(response => response.json())
+    .then(jsObject => {
+        console.log(jsObject);
+
+        let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+
+        let first = document.getElementsByClassName("first");
+        let ficon = document.getElementsByClassName("fimg");
+        let forecast = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+        
+        for (let day=0; day< forecast.length; day++) {
+            let d = new Date(forecast[day].dt_txt);
+
+            first[day].innerHTML = weekday[d.getDay()];
+
+            const fimg = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '.png';
+            const description = forecast[day].weather[0].description;
+            ficon[day].setAttribute('src', fimg);
+            ficon[day].setAttribute('alt', description);
+
+            fortemp[day].innerHTML = Math.round(forecast[day].main.temp) + "&deg;F"; 
+        }
+    });
