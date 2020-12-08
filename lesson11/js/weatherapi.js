8bd62041386fff63336db2da4d23d049c11e1ee3
madiperry <madiperry@byui.edu>
2020-12-07 18:34:36 -0600

    windchill api

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -23,7 +23,7 @@ fetch(apiURL)
         speed.innerHTML = jsObject.wind.speed;
 
         if (temp <= 50 && speed >= 3) {
-            let chill = parseFloat(35.74 + (0.6215 * temp)) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * (temp * (Math.pow (speed, 0.16))));
+            chill.innerHTML = (Math.round(parseFloat(35.74 + (0.6215 * temp)) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * (temp * (Math.pow (speed, 0.16)))))) + "&deg;F";
     } else {
           return chill ="N/A";
         }
