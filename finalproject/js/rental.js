e4e3409634dfbe7a153df30f3ac6bf63ef54f957
madiperry <madiperry@byui.edu>
2020-12-15 16:38:36 -0600

    changes to json

--- finalproject/js/rental.js
+++ finalproject/js/rental.js
@@ -1,30 +1,40 @@
 const requestURL = 'https://madiperry.github.io/finalproject/data/rental.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    // console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const price = jsonObject['price'];
-    for (let i = 0; i < prophets.length; i++ ) {
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let  = document.createElement('p');
-      let birthplace =document.createElement("p");
-      let image = document.createElement("img");
-
-      h2.textContent = prophets[i].name + ' '+ prophets[i].lastname;
-      birthdate.textContent ='Date of Birth: '+ prophets[i].birthdate;
-      birthplace.textContent = 'Place of Birth: '+ prophets[i].birthplace;
-      image.setAttribute('src', prophets[i].imageurl);
-      image.setAttribute('alt', prophets[i].name+''+prophets[i].lastname +'-'+ prophets[i].order);
-
-      card.appendChild(h2);
-      card.appendChild(birthdate);
-      card.appendChild(birthplace);
-      card.appendChild(image);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const price = jsonObject['price'];
+        for (let i = 0; i < price.length; i++ ) {
+          let card = document.createElement('section');
+          let h2 = document.createElement('h2');
+          let maxpersons = document.createElement('p');
+          let rentaltype = document.createElement('p');
+          let reshalfday = document.createElement('p');
+          let resfullday =document.createElement('p');
+          let walkhalfday =document.createElement('p');
+          let walkfullday =document.createElement('p');
+          let image = document.createElement("img");
     
-    document.querySelector('div.cards').appendChild(card);
-    }
-});
+          h2.textContent = price[i].rentaltype;
+          maxpersons.textContent ='Max Persons: '+ price[i].maxpersons;
+          reshalfday.textContent = 'Reservation Half-day: '+ price[i].reshalfday;
+          resfullday.textContent = 'Reservation Fullday: '+ price[i].resfullday;
+          walkhalfday.textContent ='Walk-in Half-day: '+ price[i].walkhalfday;
+          walkfullday.textContent = 'Walk-in Fullday: '+ price[i].walkfullday;
+          image.setAttribute('src', price[i].imagesrc);
+          image.setAttribute('alt', price[i].rentaltype);
+    
+          card.appendChild(h2);
+          card.appendChild(maxpersons);
+          card.appendChild(reshalfday);
+          card.appendChild(resfullday);
+          card.appendChild(walkhalfday);
+          card.appendChild(walkfullday);
+          card.appendChild(image);
+        
+        document.querySelector('div.cards').appendChild(card);
+        }
+    });
